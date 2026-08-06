ef682cef3cbfc60e4fac20b625b48789b6f3301d
Irma Lehikoinen <irma.lehikoi@gmail.com>
2026-08-06 15:05:33 +0300

    Siirretty projekti yhteiseen rakenteeseen

---
+++ project/brand.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | NOVI BRAND
+    |--------------------------------------------------------------------------
+    */
+
+    'name' => 'Eläinten hoitola Liperi',
+
+    'logo' => 'logo.svg',
+
+    'logo_light' => 'logo-light.svg',
+
+    'favicon' => 'favicon.ico',
+
+    'primary_color' => '#3F4F3A',
+
+    'secondary_color' => '#D8C6BD',
+
+    'accent_color' => '#2A3428',
+
+    'background_color' => '#F8F6F2',
+
+    'text_color' => '#2A3428',
+
+    'font_heading' => 'Playfair Display',
+
+    'font_body' => 'Inter',
+
+];
