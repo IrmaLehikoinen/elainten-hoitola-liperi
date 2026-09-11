73ac6553a15c8d80114fd0bbe06fcb930dc4989a
Irma Lehikoinen <irma.lehikoi@gmail.com>
2026-09-11 14:47:10 +0300

    Nosta pohja-versio 1.0.4

--- novi/config/versions.php
+++ novi/config/versions.php
@@ -5,7 +5,7 @@
 // jos vain yhtä moduulia päivitetään, muut numerot pysyvät ennallaan.
 
 return [
-    'pohja' => '1.0.3',
+    'pohja' => '1.0.4',
     'lemmikkihoitola_moduuli' => '1.1.1',
     'kurssit_moduuli' => '1.1.0',
     'ajanvaraus_moduuli' => '1.1.0',
